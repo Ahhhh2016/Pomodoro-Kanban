e900839673510a4079dc68008150e4fc37328fae
Ahhhh2016 <zuiaixinyixuan@163.com>
2025-01-03 16:38:25 +0800

    task drag&drop

--- main.ts
+++ main.ts
@@ -62,7 +62,7 @@ export class KanbanView extends ItemView {
     }
 
     getDisplayText() {
-        return 'Kanban Board';
+        return 'Pomodoro Kanban';
     }
 
     async onOpen() {
@@ -75,11 +75,40 @@ export class KanbanView extends ItemView {
             const columnEl = container.createDiv({ cls: 'kanban-column' });
             columnEl.createEl('h2', { text: column.title });
 
-            column.tasks.forEach(task => {
+			column.tasks.forEach(task => {
                 const taskEl = columnEl.createDiv({ cls: 'kanban-task' });
                 taskEl.createEl('h3', { text: task.title });
                 taskEl.createEl('p', { text: task.description });
+
+				taskEl.setAttribute('draggable', 'true');
+				taskEl.addEventListener('dragstart', (event) => {
+					event.dataTransfer.setData('text/plain', JSON.stringify(task));
+					event.dataTransfer.effectAllowed = 'move';
+				});
             });
+
+			columnEl.addEventListener('dragover', (event) => {
+				event.preventDefault();
+				event.dataTransfer.dropEffect = 'move';
+			});
+			
+			columnEl.addEventListener('drop', (event) => {
+				event.preventDefault();
+				const taskData = event.dataTransfer.getData('text/plain');
+				const task = JSON.parse(taskData);
+			
+				// Remove the task from its original column
+				initialBoard.columns.forEach(col => {
+					col.tasks = col.tasks.filter(t => t.id !== task.id);
+				});
+			
+				// Add the task to the new column
+				column.tasks.push(task);
+			
+				// Re-render the board
+				this.onOpen();
+			});
+
         });
     }
 
@@ -99,12 +128,12 @@ export default class MyPlugin extends Plugin {
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			new Notice('This is a Kanban board!');
 		});
 		
-		this.addRibbonIcon('dice', 'Greet', () => {
-			new Notice('Hello, world!');
-		  });
+		// this.addRibbonIcon('dice', 'Greet', () => {
+		// 	new Notice('Hello, world!');
+		//   });
 
 		  // Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
@@ -184,79 +213,6 @@ export default class MyPlugin extends Plugin {
         await leaf.setViewState({ type: KANBAN_VIEW_TYPE });
         this.app.workspace.revealLeaf(leaf);
     }
-
-	// async onload() {
-	// 	await this.loadSettings();
-
-	// 	// This creates an icon in the left ribbon.
-	// 	const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-	// 		// Called when the user clicks the icon.
-	// 		new Notice('This is a notice!');
-	// 	});
-		
-	// 	this.addRibbonIcon('dice', 'Greet', () => {
-	// 		new Notice('Hello, world!');
-	// 	  });
-
-	// 	  // Perform additional things with the ribbon
-	// 	ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-
-	// 	// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-	// 	const statusBarItemEl = this.addStatusBarItem();
-	// 	statusBarItemEl.setText('Status Bar Text');
-
-	// 	// This adds a simple command that can be triggered anywhere
-	// 	this.addCommand({
-	// 		id: 'open-sample-modal-simple',
-	// 		name: 'Open sample modal (simple)',
-	// 		callback: () => {
-	// 			new SampleModal(this.app).open();
-	// 		}
-	// 	});
-	// 	// This adds an editor command that can perform some operation on the current editor instance
-	// 	this.addCommand({
-	// 		id: 'sample-editor-command',
-	// 		name: 'Sample editor command',
-	// 		editorCallback: (editor: Editor, view: MarkdownView) => {
-	// 			console.log(editor.getSelection());
-	// 			editor.replaceSelection('Sample Editor Command');
-	// 		}
-	// 	});
-	// 	// This adds a complex command that can check whether the current state of the app allows execution of the command
-	// 	this.addCommand({
-	// 		id: 'open-sample-modal-complex',
-	// 		name: 'Open sample modal (complex)',
-	// 		checkCallback: (checking: boolean) => {
-	// 			// Conditions to check
-	// 			const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-	// 			if (markdownView) {
-	// 				// If checking is true, we're simply "checking" if the command can be run.
-	// 				// If checking is false, then we want to actually perform the operation.
-	// 				if (!checking) {
-	// 					new SampleModal(this.app).open();
-	// 				}
-
-	// 				// This command will only show up in Command Palette when the check function returns true
-	// 				return true;
-	// 			}
-	// 		}
-	// 	});
-
-	// 	// This adds a settings tab so the user can configure various aspects of the plugin
-	// 	this.addSettingTab(new SampleSettingTab(this.app, this));
-
-	// 	// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-	// 	// Using this function will automatically remove the event listener when this plugin is disabled.
-	// 	this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-	// 		console.log('click', evt);
-	// 	});
-
-	// 	// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-	// 	this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
-
-	// }
 
 	onunload() {
 
